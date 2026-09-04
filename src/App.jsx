b1d89b615b0a9d03d3b23dc26a28c1873ad65e33
Vipexx456 <pasuitbryan@gmail.com>
2026-09-04 17:20:35 +0800

    Completed About Me React layout

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+function App() {
+  return (
+    <div>
+      <h1>Kim Bryan Pasu-it</h1>
+      <p>Third year BSIT student at Cebu Institute of Technology – University.</p>
+      
+      <hr />
+      
+      <h2>About</h2>
+      <p>I grew up in Cebu and am currently studying IT. Now I get it why people feel amazed about an IT person.</p>
+      
+      <h2>Details</h2>
+      <p>
+        Course: BS Information Technology<br />
+        Year level: Third year<br />
+        Hometown: Dumanjug, Cebu
+      </p>
+      
+      <h2>Things I like</h2>
+      <ul>
+        <li>Eating foods</li>
+        <li>Travelling</li>
+        <li>Sleeping</li>
+      </ul>
+      
+      <h2>Reach me</h2>
+      <p>
+        kimbryan.pasu-it@cit.edu<br />
+        @pasuitbryan on most things
+      </p>
+      
+      <hr />
+      
+      <p>Made for CSIT340.</p>
+    </div>
+  )
+}
+
+export default App
